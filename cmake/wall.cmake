8d9ad3bab8108880302b113e404a6bd7637c6f64
Joel Schumacher <joelschum@gmail.com>
2025-09-05 18:31:29 +0200

    Add -Wno-missing-field-initializers

--- cmake/wall.cmake
+++ cmake/wall.cmake
@@ -1,6 +1,10 @@
-function(set_wall target)
-  if(NOT MSVC AND CMAKE_BUILD_TYPE MATCHES "Debug")
-    message(STATUS "Using -Wall/-Werror")
-    target_compile_options(${target} PRIVATE -Wall -Wextra -pedantic -Werror)
+function(mugfx_set_wall target)
+  if(MSVC)
+    target_compile_options(${target} PRIVATE /W4 /WX)
+  else()
+    target_compile_options(${target} PRIVATE -Wall -Wextra -pedantic)
+    # According to the docs, gcc should not warn about designated initializers, but sadly it does:
+    # https://gcc.gnu.org/bugzilla/show_bug.cgi?id=96868
+    target_compile_options(${target} PRIVATE -Wno-missing-field-initializers)
   endif()
 endfunction()
